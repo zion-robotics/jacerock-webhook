3b038a07f01551d4c9a20d31c6a8d16e4c804b76
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-25 22:15:02 +0100

    Add toastOptions to Toaster component

--- staff-dashboard/src/App.tsx
+++ staff-dashboard/src/App.tsx
@@ -1,3 +1,4 @@
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import Router from './router';
@@ -8,7 +9,7 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
-      <Toaster position="top-right" />
+      <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
     </QueryClientProvider>
   );
 }
