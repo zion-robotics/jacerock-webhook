b6c6f8b9dd7125109537df3dd4c2e5d89e92cecd
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-28 00:26:02 +0100

    Simplify Header component in AdminLayout
    Removed collapse functionality from Header component.

--- staff-dashboard/src/components/layout/AdminLayout.tsx
+++ staff-dashboard/src/components/layout/AdminLayout.tsx
@@ -24,14 +24,10 @@ export default function AdminLayout() {
         open={sidebarOpen}
         onClose={() => setSidebarOpen(false)}
         collapsed={sidebarCollapsed}
+        onToggleCollapse={() => setSidebarCollapsed((v) => !v)}
       />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Header
-          onMenuClick={() => setSidebarOpen(true)}
-          onCollapseClick={() => setSidebarCollapsed((v) => !v)}
-          collapsed={sidebarCollapsed}
-          title={title}
-        />
+        <Header onMenuClick={() => setSidebarOpen(true)} title={title} />
         <main className="flex-1 overflow-y-auto p-4 md:p-6">
           <Outlet />
         </main>
