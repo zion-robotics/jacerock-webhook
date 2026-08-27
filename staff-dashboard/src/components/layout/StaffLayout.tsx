a536e5b227f4a3e9d6d5fcbeb246cb35e96495af
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-28 00:26:21 +0100

    Simplify Header component in StaffLayout
    Removed collapse functionality from Header component.

--- staff-dashboard/src/components/layout/StaffLayout.tsx
+++ staff-dashboard/src/components/layout/StaffLayout.tsx
@@ -20,14 +20,10 @@ export default function StaffLayout() {
         open={sidebarOpen}
         onClose={() => setSidebarOpen(false)}
         collapsed={sidebarCollapsed}
+        onToggleCollapse={() => setSidebarCollapsed((v) => !v)}
       />
       <div className="flex-1 flex flex-col overflow-hidden">
-        <Header
-          onMenuClick={() => setSidebarOpen(true)}
-          onCollapseClick={() => setSidebarCollapsed((v) => !v)}
-          collapsed={sidebarCollapsed}
-          title={title}
-        />
+        <Header onMenuClick={() => setSidebarOpen(true)} title={title} />
         <main className="flex-1 overflow-y-auto p-4 md:p-6">
           <Outlet />
         </main>
