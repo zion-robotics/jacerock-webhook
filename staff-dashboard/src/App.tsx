3568f0004ca0bf827afc3a70b3eb6fedb79955f5
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-28 22:37:04 +0100

    Add HotAlert and NotificationBanner components

--- staff-dashboard/src/App.tsx
+++ staff-dashboard/src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import Router from './router';
 import InstallPWA from './components/ui/InstallPWA';
+import HotAlert from './components/alerts/HotAlert';
+import NotificationBanner from './components/alerts/NotificationBanner';
 
 const queryClient = new QueryClient();
 
@@ -9,8 +11,10 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
-      <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
+      <HotAlert />
+      <NotificationBanner />
       <InstallPWA />
+      <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
     </QueryClientProvider>
   );
 }
