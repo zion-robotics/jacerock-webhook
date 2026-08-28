08dee3f406811dc26502e8ef0796b99ecf4968bf
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-28 23:01:51 +0100

    Remove HotAlert and NotificationBanner components

--- staff-dashboard/src/App.tsx
+++ staff-dashboard/src/App.tsx
@@ -2,8 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import Router from './router';
 import InstallPWA from './components/ui/InstallPWA';
-import HotAlert from './components/alerts/HotAlert';
-import NotificationBanner from './components/alerts/NotificationBanner';
 
 const queryClient = new QueryClient();
 
@@ -11,8 +9,6 @@ export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
-      <HotAlert />
-      <NotificationBanner />
       <InstallPWA />
       <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
     </QueryClientProvider>
