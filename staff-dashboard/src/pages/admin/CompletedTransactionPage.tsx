4f870e0339fec40dd1b89d9baf8bd37b4c7aa1ab
Adeogun Daniel Joseph <adeogunjosephdaniel@gmail.com>
2026-08-29 21:21:16 +0000

    fix build log

--- staff-dashboard/src/pages/admin/CompletedTransactionPage.tsx
+++ staff-dashboard/src/pages/admin/CompletedTransactionPage.tsx
@@ -220,7 +220,7 @@ export default function CompletedTransactionPage() {
                 <ZoomIn className="w-3 h-3" />
                 Zoom In
               </button>
-              
+              <a
                 href={tx.receipt_url}
                 target="_blank"
                 rel="noreferrer"
